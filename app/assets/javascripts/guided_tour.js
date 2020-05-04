a5ea1c58bd361cd52ecc2f26f111de6263879900
Sean <seand7565@gmail.com>
2020-05-04 10:29:52 -0500

    Add more javascript options to guided tour
    Allows you to click on the whitebox and use the escape key to exit the tour
    Also allows for limited arrow key controls for the tour

--- app/assets/javascripts/guided_tour.js
+++ app/assets/javascripts/guided_tour.js
@@ -1,16 +1,50 @@
 $(document).ready(function(){
-  // Enables/disables the tour on click of the tour button
-  $(".tour-toggler").click(function(){
-    $(".tour-widget").toggleClass("open")
-    $($(".tour-whitebox:visible").data("selector")).toggleClass("tour-target")
-  })
-  // Handles switching to the next part of the tour, if there are multiple parts on the page
-  $(".tour-next").click(function(){
-    $(this).parent().hide()
-    $(this).parent().next().show()
-    $($(this).parent().children(".tour-whitebox").data("selector")).removeClass("tour-target")
-    $($(".tour-whitebox:visible").data("selector")).toggleClass("tour-target")
-  })
+  // Toggles the tour on click of the tour button
+  $(".tour-toggler").click(toggleTour)
+  // Disables tour when you click on the whitebox
+  $(".tour-whitebox").click(toggleTour)
+  // Disables tour when you hit the escape key
+  // Also goes to next section of tour if right arrow is clicked
+  $(document).keyup(function(e) {
+    if (e.keyCode === 27) closeTour()   // Escape key
+    if (e.keyCode === 39) goToNext()    // Right arrow
+    if (e.keyCode === 37) goToPrev()    // Left arrow
+  });
+  // Goes to the next part of the tour if the next button is clicked
+  $(".tour-next").click(goToNext)
+
   // Handles setting the selected element on pages where the tour is initialized on page load
   $($(".tour-widget.open .tour-whitebox:visible").data("selector")).toggleClass("tour-target")
+
+  // Toggles the tour on or off
+  function toggleTour() {
+    $(".tour-widget").toggleClass("open")
+    $($(".tour-whitebox:visible").data("selector")).toggleClass("tour-target")
+  }
+  // Closes tour
+  function closeTour() {
+    $(".tour-widget.open").removeClass("open")
+  }
+  // Goes to the next part of the tour
+  function goToNext() {
+    var currentSlide = $(".tour-button:visible").parent().parent()
+    if(currentSlide.next(".tour-body").length > 0){
+      currentSlide.hide()
+      currentSlide.next().show()
+      $(currentSlide.children(".tour-whitebox").data("selector")).removeClass("tour-target")
+      $($(".tour-whitebox:visible").data("selector")).toggleClass("tour-target")
+    }else{
+      $(".tour-button:visible").click()
+    }
+  }
+  // Goes to the previous slide - this does not go back a page though, whereas the goToNext function will.
+  function goToPrev(){
+    var currentSlide = $(".tour-button:visible").parent().parent()
+    if(currentSlide.prev().length > 0){
+      currentSlide.hide()
+      currentSlide.prev().show()
+      $(currentSlide.children(".tour-whitebox").data("selector")).removeClass("tour-target")
+      $($(".tour-whitebox:visible").data("selector")).toggleClass("tour-target")
+    }
+  }
 })
