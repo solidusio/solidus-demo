4d8336d7d40b2161b0a972e4a8271fce55cc4f55
Elia Schito <elia@schito.me>
2020-07-13 14:56:53 +0200

    Limit the api key to 48 chars
    This avoids failing validations in case the user is persisted to the database.

--- app/controllers/concerns/current_user.rb
+++ app/controllers/concerns/current_user.rb
@@ -18,7 +18,7 @@ module CurrentUser
       id: 0,
       spree_roles: Spree::Role.where(name: "admin"),
       email: "test@solidus.io",
-      spree_api_key: token,
+      spree_api_key: token[0...48],
     )
   end
 end
