7229f43340a75cc623701984649790cd03a2e39e
Elia Schito <elia@schito.me>
2020-07-14 16:47:39 +0200

    Rename the app to SolidusDemo

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module SolidusSample
+module SolidusDemo
   class Application < Rails::Application
     # Load application's model / class decorators
     initializer 'spree.decorators' do |app|
