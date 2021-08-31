1cf938fa2e54ffe62a480144e4f7c4a50fd8376f
Connor Ferguson <68167430+cpfergus1@users.noreply.github.com>
2021-08-31 13:42:01 -0600

    Add views for featured items
    Manage cases without any featured items
    
    Pair coding to refine the featured method.

--- app/overrides/solidus_demo/spree/home_controller/add_featured_variables_to_index.rb
+++ app/overrides/solidus_demo/spree/home_controller/add_featured_variables_to_index.rb
@@ -7,8 +7,8 @@ module SolidusDemo
         def index
           @searcher = build_searcher(params.merge(include_images: true))
           @products = @searcher.retrieve_products
-          @featured_clothing = ::Spree::Taxon.find_by(name: 'Clothing').featured
-          @featured_accessories = ::Spree::Taxon.find_by(name: 'Accessories').featured(2)
+          @featured_clothing = ::Spree::Taxon.find_by(name: 'Clothing')&.featured
+          @featured_accessories = ::Spree::Taxon.find_by(name: 'Accessories')&.featured(2)
         end
 
         ::Spree::HomeController.prepend self
