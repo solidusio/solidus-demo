ff91254d1b1290a06c12be02d20f51bb836ef4ae
Connor Ferguson <68167430+cpfergus1@users.noreply.github.com>
2021-09-08 13:10:25 -0600

    Introduce similar products for product show page
    This commit introduces a feature to find products associated with the same
    taxons as the displayed product. The feature also excludes the displayed
    product from the results. to provide a more accurate result.

--- app/monkey_patches/spree/taxon_monkey_patch.rb
+++ app/monkey_patches/spree/taxon_monkey_patch.rb
@@ -1,11 +1,15 @@
 # frozen_string_literal: true
 
 module Spree
-  module CustomQuery
+  module CustomQueries
     def featured(limit = 3)
       all_products.featured.order('RANDOM()').limit(limit)
     end
 
-    Taxon.prepend(CustomQuery)
+    def all_products_except(product_ids)
+      all_products.where.not(id: product_ids)
+    end
+
+    Taxon.prepend(CustomQueries)
   end
 end
